81c23a7652ca49f93a3e75997c16da6fbc240dfb
keith <keith_azzopardi@hotmail.com>
2021-04-27 12:39:02 +0000

    adding code for sentiment analysis

--- sentimentAnalyzeServer/sentimentAnalyzerServer.js
+++ sentimentAnalyzeServer/sentimentAnalyzerServer.js
@@ -50,7 +50,22 @@ app.get("/url/emotion", (req,res) => {
 });
 
 app.get("/url/sentiment", (req,res) => {
-    return res.send("url sentiment for "+req.query.url);
+    const nlu = getNLUInstance();
+    const analyzeParams = {
+        'url': req.query.url,
+        'features': {
+            'sentiment': {
+                'document': true 
+            }
+        }
+    };
+    nlu.analyze(analyzeParams)
+        .then(analysisResults => {
+            return res.send(analysisResults.result.sentiment.document.label);
+        })
+        .catch(err => {
+           return res.status(400).json({ error: err.toString() });
+        });
 });
 
 app.get("/text/emotion", (req,res) => {
@@ -74,7 +89,23 @@ app.get("/text/emotion", (req,res) => {
 });
 
 app.get("/text/sentiment", (req,res) => {
-    return res.send("text sentiment for "+req.query.text);
+    const nlu = getNLUInstance();
+    const analyzeParams = {
+        'language': 'en',
+        'text': req.query.text,
+        'features': {
+            'sentiment': {
+                'document': true 
+            }
+        }
+    };
+    nlu.analyze(analyzeParams)
+        .then(analysisResults => {
+            return res.send(analysisResults.result.sentiment.document.label);
+        })
+        .catch(err => {
+            return res.status(400).json({ error: err.toString() });
+        });
 });
 
 let server = app.listen(8080, () => {
